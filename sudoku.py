3b9499efbbf7cf289f35c842bd540b4eb8f3e855
Philipp Weber <Philipp.Weber@hs-augsburg.de>
2016-04-30 01:31:14 +0200

    - changed "number" to "digit" in the whole sudoku.py to make the code unambiguous  - Sudoku._hidden_twin_step() and Sudoku._subgroup_exclusion() now reduce    possibilities as often as possible, not just once  - added documentation to Sudoku._subgroup_exclusion()  - minor change to Sudoku._subgroup_exclusion for better readablility  - fixed issue with Sudolu_hidden_twin()

--- sudoku.py
+++ sudoku.py
@@ -9,7 +9,7 @@ class Sudoku:
     Attributes:
         grid            a 9x9 list of integer-lists for holding the the puzzle numbers,
                         zero symbolizes no value
-        solved          true when sudoku has been successfully solved
+        solved          True when sudoku has been successfully solved
 
         _boxes          3x3 list of lists of sets, containing each number contained
                         in a box (a box is a 3x3 segment of the puzzle)
@@ -23,11 +23,11 @@ class Sudoku:
         _possCBoxes     same as _possCRows, but for boxes
     """
     def __init__(self, other=None):
-        self.grid =         [[0]*9 for _ in range(9)]
-        self._boxes =       [[set() for _ in range(3)] for _ in range(3)]
-        self._rows =         [set() for _ in range(9)]
-        self._columns =      [set() for _ in range(9)]
-        self._poss =        [[set() for _ in range(9)] for _ in range(9)]
+        self.grid =     [[0]*9 for _ in range(9)]
+        self._boxes =  [[set() for _ in range(3)] for _ in range(3)]
+        self._rows =    [set() for _ in range(9)]
+        self._columns = [set() for _ in range(9)]
+        self._poss =   [[set() for _ in range(9)] for _ in range(9)]
         self._possCRows =    [[0] * 9 for _ in range(9)]
         self._possCColumns = [[0] * 9 for _ in range(9)]
         self._possCBoxes =  [[[0] * 9 for _ in range(3)] for _ in range(3)]
@@ -54,8 +54,8 @@ class Sudoku:
     def read_string(self, in_string):
         """
         read in sudoku from string
-        :param in_string:   string with numbers which are written row by row,
-                            a ',' indicates the next number,
+        :param in_string:   string with digits which are written row by row,
+                            a ',' indicates the next digit,
                             a ';' indicates a new row. zero symbolizes no value
         """
         x = 0
@@ -74,7 +74,7 @@ class Sudoku:
 
     def recursive_solve(self):
         """
-        Solves the sudoku with regular strategies until it can not find any new numbers.
+        Solves the sudoku with regular strategies until it can not find any new digits.
         Then it tries to find the solution by recursively "brute-forcing" the cells with
         the fewest possibilities. If the sudoku is ambiguous, it uses one possible solution.
         :raise IncorrectSudokuException: If the input sudoku has no solution.
@@ -91,7 +91,7 @@ class Sudoku:
                 except IncorrectSudokuException:
                     pass
                 if s.solved:
-                    print("'guessed' %i out of %i numbers in %i %i" % (n, len(min_set), x, y))
+                    print("'guessed' %i out of %i digits in %i %i" % (n, len(min_set), x, y))
                     self.grid = s.grid
                     self.solved = True
                     break
@@ -110,7 +110,7 @@ class Sudoku:
 
     def _find_min_poss(self):
         """
-        find one of the cells with the fewest number of possibilities
+        find one of the cells with the fewest count of possibilities
         :return: tuple of indexes (x, y)
         """
         minx, miny = 0, 0
@@ -124,7 +124,7 @@ class Sudoku:
 
     def __str__(self):
         """
-        :return: the comma separated numbers row by row
+        :return: the comma separated digits row by row
         """
         out = ""
         for y in range(9):
@@ -141,7 +141,7 @@ class Sudoku:
         the Sub-Group exclusion rule and
         the Hidden Twin exclusion rule
         (see http://www.sudokudragon.com/sudokustrategy.htm)
-        :return: true if a new number has been found
+        :return: True if a new digit has been found
         """
         self._update_units()
         self._fill_poss()
@@ -198,6 +198,20 @@ class Sudoku:
                     self._columns[x].add(n)
                     self._boxes[x // 3][y // 3].add(n)
 
+    def _update_poss_counts(self):
+        """
+        update the values in _possCRows, _possCColumns and _possCBoxes
+        """
+        self._reset_poss_counts()
+        for x in range(9):
+            for y in range(9):
+                if self.grid[x][y] == 0:
+                    for n in self._poss[x][y]:
+                        self._possCRows[y][n - 1] += 1
+                        self._possCColumns[x][n - 1] += 1
+                        self._possCBoxes[x // 3][y // 3][n - 1] += 1
+
+
     def _fill_poss(self):
         """
         update the values in _poss
@@ -214,9 +228,9 @@ class Sudoku:
 
     def _hidden_twin(self):
         """
-        try apply the hidden twin rule once to either a row, column or a box
+        try apply the hidden twin rule to rows, columns or boxes
         the rule itself if implemented in _hidden_twin_step
-        :return: true if the rule was applied
+        :return: True if the rule was applied
         """
         # what follows are the functions used on the offsets
         # to iterate on columns, rows and boxes
@@ -225,33 +239,37 @@ class Sudoku:
         def box_y(i, j): return (i % 3) * 3 + j % 3
 
         def col_x(i, j): return i
-
+   
         def col_y(i, j): return j
 
         def row_x(i, j): return j
 
         def row_y(i, j): return i
 
+        changed = False
         for i in range(9):
             for j in range(9):
-                if self._hidden_twin_step(col_x(i, j), col_y(i, j)):
-                    return True
-                elif self._hidden_twin_step(row_x(i, j), row_y(i, j)):
-                    return True
-                elif self._hidden_twin_step(box_x(i, j), box_y(i, j)):
-                    return True
-        return False
+                if self._hidden_twin_step(col_x, col_y, i, j):
+                    changed = True
+                if self._hidden_twin_step(row_x, row_y, i, j):
+                    changed = True
+                if self._hidden_twin_step(box_x, box_y, i, j):
+                    changed = True
+        return changed
 
     def _hidden_twin_step(self, xfunc, yfunc, i, j):
         """
-        apply the Hidden twin rule described at www.sudokudragon.com/sudokustrategy.htm
+        Apply the Hidden Twin rule described at www.sudokudragon.com/sudokustrategy.htm.
+        If there are 2 cells inside a container(row/column/box) witch have the same pair
+        of possibilities, the to digits in the pair can not be anywhere else in the
+        container.
         :param xfunc: function that defines how the i and j parameters should be
                       transformed to get the x coordinate. This parameter makes it
                       possible to apply this function to rows, columns and boxes
         :param yfunc: same as xfunc for the y coordinate
         :param i: first offset of the cell
         :param j: second offset of the cell
-        :return: true if the rule was successfully applied
+        :return: True if the rule was successfully applied
         """
         changed = False
         x, y = xfunc(i, j), yfunc(i, j)
@@ -262,7 +280,7 @@ class Sudoku:
                 if self._poss[x][y] == self._poss[xfunc(i, j2)][yfunc(i, j2)]:
                     # cell with same pair is cell (i, j2)
                     pair = self._poss[x][y]
-                    # remove all occurrences of the paired numbers in the sequence
+                    # remove all occurrences of the paired digits in the sequence
                     # which are not the pairs themselves
                     for j3 in range(9):
                         if j3 != j and j3 != j2:
@@ -274,55 +292,56 @@ class Sudoku:
 
     def _subgroup_exclusion(self):
         """
-        apply the Subgroup exclusion rule(see www.sudokudragon.com/sudokustrategy.htm)
-        :return: whether the rule was successfully applied
+        Apply the Subgroup exclusion rule. (see www.sudokudragon.com/sudokustrategy.htm)
+        Must be applied repeatedly since the possibility counts do not get updated inside
+        the function.
+
+        Subgroups are 3-cell columns or rows inside boxes
+        Are the cells of  a subgroup the only place in a box where a digit can be,
+        then the digit CAN NOT be anywhere in the full row/column the subgroup is part of
+        :return: True if the rule was successfully applied
         """
+        changed = False
+        # for each row/column
         for i in range(9):
+            # for each box that crosses the row/column
             for boxIndex in range(3):
-                # check columns intersecting with boxes
+                # ----- check columns intersecting with boxes -----
+                # poss_c[digit - 1] = count of possible cells for "digit"
+                #                      in the current subgroup
                 poss_c = [0]*9
+                # for each cell in the subgroup
                 for k in range(3):
+                    # sum up the count of possibilities for each digit in the subgroup
                     for n in self._poss[i][3*boxIndex+k]:
                         poss_c[n-1] += 1
-                    for index, count in enumerate(poss_c):
-                        if count != 0 and count == self._possCBoxes[i//3][boxIndex][index]:
-                            n = index + 1
+                    for digit, count in enumerate(poss_c, 1):
+                        # if all possibilities of a digit are in the subgroup, remove the
+                        # digit from the possibilities of the cells in the column and outside the box
+                        if count != 0 and count == self._possCBoxes[i//3][boxIndex][digit-1]:
                             changed = False
                             for y in range(9):
-                                if y//3 != boxIndex and n in self._poss[i][y]:
+                                if y//3 != boxIndex and digit in self._poss[i][y]:
                                     changed = True
-                                    self._poss[i][y].remove(n)
-                            if changed:
-                                # print("reduced %i in intersecting column %i box %i %i" % (n, i, i//3, boxIndex))
-                                return True
+                                    self._poss[i][y].remove(digit)
 
-                # check for rows intersecting with boxes
+                # ----- check for rows intersecting with boxes -----
+                # for each cell in the subgroup
                 poss_c = [0]*9
                 for k in range(3):
+                    # sum up the count of possibilities for each digit in the subgroup
                     for n in self._poss[3*boxIndex+k][i]:
                         poss_c[n-1] += 1
-                    for index, count in enumerate(poss_c):
-                        if count != 0 and count == self._possCBoxes[boxIndex][i//3][index]:
-                            n = index + 1
+                    for digit, count in enumerate(poss_c, 1):
+                        # if all possibilities of a digit are in the subgroup, remove the
+                        # digit from the possibilities of the cells in the row and outside the box
+                        if count != 0 and count == self._possCBoxes[boxIndex][i//3][digit-1]:
                             changed = False
                             for x in range(9):
-                                if x//3 != boxIndex and n in self._poss[x][i]:
+                                if x//3 != boxIndex and digit in self._poss[x][i]:
                                     changed = True
-                                    self._poss[x][i].remove(n)
-                            if changed:
-                                # print("reduced %i in intersecting row %i box %i %i" % (n, i, boxIndex, i//3))
-                                return True
-        return False
-
-    def _update_poss_counts(self):
-        self._reset_poss_counts()
-        for x in range(9):
-            for y in range(9):
-                if self.grid[x][y] == 0:
-                    for n in self._poss[x][y]:
-                        self._possCRows[y][n - 1] += 1
-                        self._possCColumns[x][n - 1] += 1
-                        self._possCBoxes[x // 3][y // 3][n - 1] += 1
+                                    self._poss[x][i].remove(digit)
+        return changed
 
 if __name__ == "__main__":
     sudoku = Sudoku()
